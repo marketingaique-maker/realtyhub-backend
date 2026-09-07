65d94222ad6bcaf11db48a88e9b12a41f19df671
RealtyHub <marketing.aique@gmail.com>
2026-09-07 19:08:04 +0530

    fix indentation in build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-User.objects.create_superuser('admin','marketing.aique@gmail.com','Realtyhubthunderbird3')
+    User.objects.create_superuser('admin','marketing.aique@gmail.com','Realtyhubthunderbird3')
 "
